dba70cf92146f927b1ed6c7ef6a8de3ad73ecfcf
zqmillet <zqmillet@qq.com>
2019-03-24 11:27:01 +0800

    add some files

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import fpdf
 import re
 import os
 
-from xterm_color_to_rgb import xterm_color_to_rgb
-
 def get_panel_ascii_code(panel_name):
     command = ['tmux', 'capture-pane', '-t', panel_name, '-e', '-J', '-p']
     result = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -20,6 +18,9 @@ class STATUS:
     END = 2
 
 class CONTROL:
+    UNDER_LINE = 4
+    BOLD_ON = 1
+    CANCEL = 0
     SET_FORE_COLOR = 38
     SET_BACK_COLOR = 48
     SET_DEFAULT_FORE_COLOR = 39
@@ -45,38 +46,345 @@ class CONTROL:
         47: (255,255,255)
     }
 
+class ColorMap(dict):
+    def __init__(self):
+        map = {
+            0:   (0, 0, 0),
+            1:   (128, 0, 0),
+            2:   (0, 128, 0),
+            3:   (128, 128, 0),
+            4:   (0, 0, 128),
+            5:   (128, 0, 128),
+            6:   (0, 128, 128),
+            7:   (192, 192, 192),
+            8:   (128, 128, 128),
+            9:   (255, 0, 0),
+            10:  (0, 255, 0),
+            11:  (255, 255, 0),
+            12:  (0, 0, 255),
+            13:  (255, 0, 255),
+            14:  (0, 255, 255),
+            15:  (255, 255, 255),
+            16:  (0, 0, 0),
+            17:  (0, 0, 95),
+            18:  (0, 0, 135),
+            19:  (0, 0, 175),
+            20:  (0, 0, 215),
+            21:  (0, 0, 255),
+            22:  (0, 95, 0),
+            23:  (0, 95, 95),
+            24:  (0, 95, 135),
+            25:  (0, 95, 175),
+            26:  (0, 95, 215),
+            27:  (0, 95, 255),
+            28:  (0, 135, 0),
+            29:  (0, 135, 95),
+            30:  (0, 135, 135),
+            31:  (0, 135, 175),
+            32:  (0, 135, 215),
+            33:  (0, 135, 255),
+            34:  (0, 175, 0),
+            35:  (0, 175, 95),
+            36:  (0, 175, 135),
+            37:  (0, 175, 175),
+            38:  (0, 175, 215),
+            39:  (0, 175, 255),
+            40:  (0, 215, 0),
+            41:  (0, 215, 95),
+            42:  (0, 215, 135),
+            43:  (0, 215, 175),
+            44:  (0, 215, 215),
+            45:  (0, 215, 255),
+            46:  (0, 255, 0),
+            47:  (0, 255, 95),
+            48:  (0, 255, 135),
+            49:  (0, 255, 175),
+            50:  (0, 255, 215),
+            51:  (0, 255, 255),
+            52:  (95, 0, 0),
+            53:  (95, 0, 95),
+            54:  (95, 0, 135),
+            55:  (95, 0, 175),
+            56:  (95, 0, 215),
+            57:  (95, 0, 255),
+            58:  (95, 95, 0),
+            59:  (95, 95, 95),
+            60:  (95, 95, 135),
+            61:  (95, 95, 175),
+            62:  (95, 95, 215),
+            63:  (95, 95, 255),
+            64:  (95, 135, 0),
+            65:  (95, 135, 95),
+            66:  (95, 135, 135),
+            67:  (95, 135, 175),
+            68:  (95, 135, 215),
+            69:  (95, 135, 255),
+            70:  (95, 175, 0),
+            71:  (95, 175, 95),
+            72:  (95, 175, 135),
+            73:  (95, 175, 175),
+            74:  (95, 175, 215),
+            75:  (95, 175, 255),
+            76:  (95, 215, 0),
+            77:  (95, 215, 95),
+            78:  (95, 215, 135),
+            79:  (95, 215, 175),
+            80:  (95, 215, 215),
+            81:  (95, 215, 255),
+            82:  (95, 255, 0),
+            83:  (95, 255, 95),
+            84:  (95, 255, 135),
+            85:  (95, 255, 175),
+            86:  (95, 255, 215),
+            87:  (95, 255, 255),
+            88:  (135, 0, 0),
+            89:  (135, 0, 95),
+            90:  (135, 0, 135),
+            91:  (135, 0, 175),
+            92:  (135, 0, 215),
+            93:  (135, 0, 255),
+            94:  (135, 95, 0),
+            95:  (135, 95, 95),
+            96:  (135, 95, 135),
+            97:  (135, 95, 175),
+            98:  (135, 95, 215),
+            99:  (135, 95, 255),
+            100: (135, 135, 0),
+            101: (135, 135, 95),
+            102: (135, 135, 135),
+            103: (135, 135, 175),
+            104: (135, 135, 215),
+            105: (135, 135, 255),
+            106: (135, 175, 0),
+            107: (135, 175, 95),
+            108: (135, 175, 135),
+            109: (135, 175, 175),
+            110: (135, 175, 215),
+            111: (135, 175, 255),
+            112: (135, 215, 0),
+            113: (135, 215, 95),
+            114: (135, 215, 135),
+            115: (135, 215, 175),
+            116: (135, 215, 215),
+            117: (135, 215, 255),
+            118: (135, 255, 0),
+            119: (135, 255, 95),
+            120: (135, 255, 135),
+            121: (135, 255, 175),
+            122: (135, 255, 215),
+            123: (135, 255, 255),
+            124: (175, 0, 0),
+            125: (175, 0, 95),
+            126: (175, 0, 135),
+            127: (175, 0, 175),
+            128: (175, 0, 215),
+            129: (175, 0, 255),
+            130: (175, 95, 0),
+            131: (175, 95, 95),
+            132: (175, 95, 135),
+            133: (175, 95, 175),
+            134: (175, 95, 215),
+            135: (175, 95, 255),
+            136: (175, 135, 0),
+            137: (175, 135, 95),
+            138: (175, 135, 135),
+            139: (175, 135, 175),
+            140: (175, 135, 215),
+            141: (175, 135, 255),
+            142: (175, 175, 0),
+            143: (175, 175, 95),
+            144: (175, 175, 135),
+            145: (175, 175, 175),
+            146: (175, 175, 215),
+            147: (175, 175, 255),
+            148: (175, 215, 0),
+            149: (175, 215, 95),
+            150: (175, 215, 135),
+            151: (175, 215, 175),
+            152: (175, 215, 215),
+            153: (175, 215, 255),
+            154: (175, 255, 0),
+            155: (175, 255, 95),
+            156: (175, 255, 135),
+            157: (175, 255, 175),
+            158: (175, 255, 215),
+            159: (175, 255, 255),
+            160: (215, 0, 0),
+            161: (215, 0, 95),
+            162: (215, 0, 135),
+            163: (215, 0, 175),
+            164: (215, 0, 215),
+            165: (215, 0, 255),
+            166: (215, 95, 0),
+            167: (215, 95, 95),
+            168: (215, 95, 135),
+            169: (215, 95, 175),
+            170: (215, 95, 215),
+            171: (215, 95, 255),
+            172: (215, 135, 0),
+            173: (215, 135, 95),
+            174: (215, 135, 135),
+            175: (215, 135, 175),
+            176: (215, 135, 215),
+            177: (215, 135, 255),
+            178: (215, 175, 0),
+            179: (215, 175, 95),
+            180: (215, 175, 135),
+            181: (215, 175, 175),
+            182: (215, 175, 215),
+            183: (215, 175, 255),
+            184: (215, 215, 0),
+            185: (215, 215, 95),
+            186: (215, 215, 135),
+            187: (215, 215, 175),
+            188: (215, 215, 215),
+            189: (215, 215, 255),
+            190: (215, 255, 0),
+            191: (215, 255, 95),
+            192: (215, 255, 135),
+            193: (215, 255, 175),
+            194: (215, 255, 215),
+            195: (215, 255, 255),
+            196: (255, 0, 0),
+            197: (255, 0, 95),
+            198: (255, 0, 135),
+            199: (255, 0, 175),
+            200: (255, 0, 215),
+            201: (255, 0, 255),
+            202: (255, 95, 0),
+            203: (255, 95, 95),
+            204: (255, 95, 135),
+            205: (255, 95, 175),
+            206: (255, 95, 215),
+            207: (255, 95, 255),
+            208: (255, 135, 0),
+            209: (255, 135, 95),
+            210: (255, 135, 135),
+            211: (255, 135, 175),
+            212: (255, 135, 215),
+            213: (255, 135, 255),
+            214: (255, 175, 0),
+            215: (255, 175, 95),
+            216: (255, 175, 135),
+            217: (255, 175, 175),
+            218: (255, 175, 215),
+            219: (255, 175, 255),
+            220: (255, 215, 0),
+            221: (255, 215, 95),
+            222: (255, 215, 135),
+            223: (255, 215, 175),
+            224: (255, 215, 215),
+            225: (255, 215, 255),
+            226: (255, 255, 0),
+            227: (255, 255, 95),
+            228: (255, 255, 135),
+            229: (255, 255, 175),
+            230: (255, 255, 215),
+            231: (255, 255, 255),
+            232: (8, 8, 8),
+            233: (18, 18, 18),
+            234: (28, 28, 28),
+            235: (38, 38, 38),
+            236: (48, 48, 48),
+            237: (58, 58, 58),
+            238: (68, 68, 68),
+            239: (78, 78, 78),
+            240: (88, 88, 88),
+            241: (98, 98, 98),
+            242: (108, 108, 108),
+            243: (118, 118, 118),
+            244: (128, 128, 128),
+            245: (138, 138, 138),
+            246: (148, 148, 148),
+            247: (158, 158, 158),
+            248: (168, 168, 168),
+            249: (178, 178, 178),
+            250: (188, 188, 188),
+            251: (198, 198, 198),
+            252: (208, 208, 208),
+            253: (218, 218, 218),
+            254: (228, 228, 228),
+            255: (238, 238, 238)
+        }
+
+        for key, value in map.items():
+            self[key] = value
+
+    def set_black(self, color):
+        # self[0] = color
+        # self[16] = color
+        pass
+
 class PDFFile(object):
     __file = None
     __default_fore_color = None
     __default_back_color = None
+    __bold_fore_color = None
+    __bold_back_color = None
     __font_name = None
     __font_size = None
+    __char_width = None
+    __char_height = None
+    __color_map = None
+    __fore_color = None
+    __back_color = None
 
     def __init__(
         self,
+        ascii_code,
         default_fore_color,
         default_back_color,
+        bold_fore_color = None,
+        bold_back_color = None,
         font_path = './fonts/consolas.ttf',
-        font_size = 5
+        font_size = 5.3,
+        char_width = 1,
+        char_height = 2
     ):
+        ascii_code = ascii_code.split('\n')
+        maximum_line_length = get_maximum_length(ascii_code)
+        ascii_code = delete_blank_lines(ascii_code)
+        line_number = len(ascii_code)
+
         font_name = os.path.basename(font_path)
         font_name = os.path.splitext(font_name)[0]
 
-        self.__file = fpdf.FPDF()
+        self.__bold_fore_color = bold_fore_color if not bold_fore_color is None else default_back_color
+        self.__bold_back_color = bold_back_color if not bold_back_color is None else default_fore_color
+
+        self.__color_map = ColorMap()
+        self.__color_map.set_black(default_back_color)
+
+        self.__char_width = char_width
+        self.__char_height = char_height
+
+        self.__file = fpdf.FPDF(format = (maximum_line_length * self.__char_width, line_number * self.__char_height))
         self.__file.add_page()
+        self.__file.set_xy(0, 0)
+        self.__file.set_margins(0, 0, 0)
+        self.__file.set_auto_page_break(False)
         self.__file.add_font(font_name, fname = font_path, uni = True)
+
         self.__default_fore_color = default_fore_color
         self.__default_back_color = default_back_color
+        self.__fore_color = default_fore_color
+        self.__back_color = default_back_color
+
         self.set_font(font_name = font_name, font_size = font_size)
+
+        for line in ascii_code:
+            self.write_line(line, maximum_line_length)
 
     def set_back_color(self, r, g, b):
         self.__file.set_fill_color(r, g, b)
+        self.__back_color = (r, g, b)
 
     def set_fore_color(self, r, g, b):
         self.__file.set_text_color(r, g, b)
+        self.__fore_color = (r, g, b)
 
-    def set_font(self, font_name, font_size):
-        self.__file.set_font(font_name, '', font_size)
+    def set_font(self, font_name, font_size, style = ''):
+        self.__file.set_font(font_name, style, font_size)
         self.__font_name = font_name
         self.__font_size = font_size
 
@@ -84,10 +392,10 @@ class PDFFile(object):
         self.set_back_color(*self.__default_back_color)
         self.set_fore_color(*self.__default_fore_color)
 
-    def add_char(self, char, end_line = False, fill = True, border = False, width = 1, height = 2, align = 'C'):
+    def add_char(self, char, end_line = False, fill = True, border = False, align = 'C'):
         border = 1  if border else 0
         end_line = 1 if end_line else 0
-        self.__file.cell(width, height, char, border, end_line, fill = True, align = align)
+        self.__file.cell(self.__char_width, self.__char_height, char, border, end_line, fill = True, align = align)
 
     def write_line(self, ascii_code, maximum_line_length):
         status = STATUS.IDEL
@@ -100,6 +408,7 @@ class PDFFile(object):
                     status = STATUS.BEGIN
                     continue
                 else:
+                    # print(offset, maximum_line_length - 1)
                     if offset == maximum_line_length - 1:
                         self.add_char(char, end_line = True)
                         offset = 0
@@ -114,7 +423,19 @@ class PDFFile(object):
                 else:
                     control_sequence += char
             else:
-                import pdb; pdb.set_trace()
+                pass
+
+        if offset == 0 and len(ascii_code) > 0:
+            return
+
+        fore_color = self.__fore_color
+        back_color = self.__back_color
+        self.set_default_color()
+        for _ in range(maximum_line_length - offset - 1):
+            self.add_char(' ', end_line = False)
+        self.add_char(' ', end_line = True)
+        self.set_fore_color(*fore_color)
+        self.set_back_color(*back_color)
 
     def execute(self, control_sequence):
         control_sequence = [int(command) for command in control_sequence[1:].split(';')]
@@ -129,19 +450,33 @@ class PDFFile(object):
                 self.set_back_color(*CONTROL.SYSTEM_BACK_COLOR[command])
                 index += 1
             elif command == CONTROL.SET_FORE_COLOR:
-                self.set_fore_color(*xterm_color_to_rgb(control_sequence[index + 2]))
+                self.set_fore_color(*self.__color_map[control_sequence[index + 2]])
                 index += 3
             elif command == CONTROL.SET_BACK_COLOR:
-                self.set_back_color(*xterm_color_to_rgb(control_sequence[index + 2]))
+                self.set_back_color(*self.__color_map[control_sequence[index + 2]])
                 index += 3
+            elif command == CONTROL.BOLD_ON:
+                self.bold_on()
+                index += 1
             elif command == CONTROL.SET_DEFAULT_FORE_COLOR:
                 self.set_fore_color(*self.__default_fore_color)
                 index += 1
             elif command == CONTROL.SET_DEFAULT_BACK_COLOR:
                 self.set_back_color(*self.__default_back_color)
                 index += 1
+            elif command == CONTROL.CANCEL:
+                self.set_font(self.__font_name, self.__font_size, '')
+                self.set_default_color()
+                index += 1
+            elif command == CONTROL.UNDER_LINE:
+                self.set_font(self.__font_name, self.__font_size, 'U')
+                index += 1
             else:
                 index += 1
+
+    def bold_on(self):
+        self.set_fore_color(*self.__bold_fore_color)
+        self.set_back_color(*self.__bold_back_color)
 
     def save(self, file_path):
         self.__file.set_margins(0, 0, 0)
@@ -166,17 +501,15 @@ def testcases():
         file.write(ascii_code)
 
     pdf_file = PDFFile(
-        default_fore_color = (112, 130, 132),
-        default_back_color = (10, 35, 44),
-        font_path = './fonts/consolas.ttf'
+        # default_fore_color = (112, 130, 132),
+        # default_back_color = (10, 35, 44),
+        default_fore_color = (255, 255, 255),
+        default_back_color = (0, 0, 0),
+        bold_fore_color = (255,255,0),
+        bold_back_color = (0, 0, 0),
+        font_path = './fonts/consolas.ttf',
+        ascii_code = ascii_code
     )
-
-    ascii_code = ascii_code.split('\n')
-    maximum_line_length = get_maximum_length(ascii_code)
-    ascii_code = delete_blank_lines(ascii_code)
-
-    for line in ascii_code:
-        pdf_file.write_line(line, maximum_line_length)
 
     pdf_file.save('./main.pdf')
 
