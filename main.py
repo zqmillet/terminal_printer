b58ea6d8153132658fc66d9dc06ee35c2ab1456d
zqmillet <zqmillet@qq.com>
2019-04-03 23:35:18 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from utilities import ArgumentParser
 from utilities import PDFFile
 from utilities import FLAG, CONTROL, STATUS
 
-def get_panel_ascii_code(panel_name):
-    command = ['tmux', 'capture-pane', '-t', panel_name, '-e', '-J', '-p']
+def get_pane_ascii_code(pane_name):
+    command = ['tmux', 'capture-pane', '-t', pane_name, '-e', '-J', '-p']
     result = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     return result.stdout.read().decode('utf8')
 
@@ -82,8 +82,9 @@ def parse_arguments():
     return argument_parser.parse_args(sys.argv[1:])
 
 def testcases():
-    # arguments = parse_arguments()
-    ascii_code = get_panel_ascii_code('zhangqi:0.0')
+    arguments = parse_arguments()
+
+    ascii_code = get_panel_ascii_code(arguments.pane)
     with open('tmux.txt', 'w') as file:
         file.write(ascii_code)
 
